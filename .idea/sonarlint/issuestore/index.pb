2b88aee4361d5819e965fe68bef93544393f9d70
FranciscoDadone <dadonefran@gmail.com>
2022-10-07 23:18:41 -0300

    App terminada

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -2,3 +2,7 @@
 :
 
 .gitignore,a/5/a5cc2925ca8258af241be7e5b0381edf30266302
+7
+main.py,f/1/f1bdda93d9a278e358509d498e17d97764c1fb29
+<
+.env.example,d/4/d4dae00d11854b35292c2b8a30515a9a0aa2d871
--- main.py
+++ main.py
@@ -2,15 +2,13 @@ import sys
 import dropbox
 from dropbox.files import WriteMode
 from dropbox.exceptions import ApiError, AuthError
-import datetime
 import os
 from dotenv import load_dotenv
 import shutil
-import subprocess
+import datetime
 
 
 dt = datetime.datetime.today()
-LOCAL_FILE = '/home/franciscodadone/Dev/sistema_de_gestion/README.md'
 BACKUP_PATH = f"/{dt.day}-{dt.month}-{dt.year}"
 
 
@@ -34,20 +32,63 @@ def backup(local_file, backup_path):
                 sys.exit()
 
 
-def local_backup():
-    subprocess.run()
+def local_purge():
+    backup_path_list = os.listdir(backup_path)
+    while len(backup_path_list) >= int(os.getenv('HOLD_BACKUPS')):
+        oldest = datetime.datetime.now()
+        oldest_str = ''
+        for file in backup_path_list:
+            str_date = file.replace('.zip', '')
+            date = datetime.datetime.strptime(str_date, "%d-%m-%Y_%H:%M:%S")
+            if oldest > date:
+                oldest = date
+                oldest_str = file
+        os.remove(backup_path + '/' + oldest_str)
+        backup_path_list.remove(oldest_str)
+
+
+def remote_purge():
+    dropbox_path = os.getenv('DROPBOX_PATH')
+    backup_path_list = dbx.files_list_folder(dropbox_path).entries
+    while len(backup_path_list) >= int(os.getenv('HOLD_BACKUPS')):
+        oldest = datetime.datetime.now()
+        oldest_str = ''
+        for file in backup_path_list:
+            str_date = file.name.replace('.zip', '')
+            date = datetime.datetime.strptime(str_date, "%d-%m-%Y_%H:%M:%S")
+            if oldest > date:
+                oldest = date
+                oldest_str = file
+        dbx.files_delete_v2(dropbox_path + '/' + oldest_str.name)
+        backup_path_list.remove(oldest_str)
 
 
 if __name__ == '__main__':
     load_dotenv()
+    backup_path = os.getenv('BACKUP_PATH').replace('TEMP_FOLDER', os.getenv('TEMP_FOLDER'))
+    temp_path = os.getenv('TEMP_FOLDER').replace('BACKUP_PATH', backup_path)
+    pre_backup_cmds = os.getenv('PRE_BACKUP_CMDS').replace('BACKUP_PATH', backup_path).replace('TEMP_FOLDER', temp_path).split(',')
 
+    if not os.path.isdir(backup_path):
+        os.mkdir(backup_path)
 
+    local_purge()
 
-    print("Creating a Dropbox object...")
+    if not os.path.isdir(temp_path):
+        os.mkdir(temp_path)
+
+    for cmd in pre_backup_cmds:
+        os.system(cmd)
+
+    compressed_path = backup_path + '/' + datetime.datetime.now().strftime("%d-%m-%Y_%H:%M:%S")
+    shutil.make_archive(compressed_path, 'zip', temp_path)
+    shutil.rmtree(temp_path)
+
     dbx = dropbox.Dropbox(os.getenv("ACCESS_TOKEN"))
     try:
         dbx.users_get_current_account()
     except AuthError:
         sys.exit("ERROR: Invalid access token; try re-generating an access token from the app console on the web.")
-    backup(LOCAL_FILE, BACKUP_PATH)
+    remote_purge()
+    backup(compressed_path + '.zip', os.getenv('DROPBOX_PATH') + '/' + compressed_path.split('/')[-1] + '.zip')
 print("Done!")
